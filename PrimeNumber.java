acbff548a2f1b463f510f62371550cb4262ff7fe
itsepsita <77191477+itsepsita@users.noreply.github.com>
2023-11-23 19:51:48 +0530

    PrimeNumber.java

---
+++ PrimeNumber.java
@@ -0,0 +1,22 @@
+import java.util.Scanner; 
+ public class PrimeNumber 
+ { 
+                 public static void main(String args[]) 
+              { 
+                   int num,b,c; 
+                   Scanner s=new Scanner(System.in);
+                   num =s.nextInt(); 
+                   b=1; 
+                   c=0; 
+                    while(b<= num) 
+                       { 
+                           if((num%b)==0) 
+                              c=c+1; 
+                              b++; 
+                       } 
+                        if(c==2) 
+                        System.out.println(num +" is a prime number"); 
+                        else 
+                        System.out.println(num +" is not a prime number"); 
+              } 
+ } 
