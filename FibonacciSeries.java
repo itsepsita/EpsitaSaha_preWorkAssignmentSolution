4d174dc8ca46a4d6dd57de82aacbc2de2c51a3af
itsepsita <77191477+itsepsita@users.noreply.github.com>
2023-11-23 19:57:06 +0530

    Prints Fibonacci Series for a given number

---
+++ FibonacciSeries.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Fibonacci 
+{
+    public static void main(String[] args) 
+    {
+        int n, a = 0, b = 0, c = 1;
+        Scanner s = new Scanner(System.in);
+        n = s.nextInt();
+        System.out.print("The Fibonacci series is:");
+        for(int i = 1; i <= n; i++)
+        {
+            a = b;
+            b = c;
+            c = a + b;
+            System.out.print(a+" ");
+        }
+    }
+}
